abb30d8851d4065b09b1e0596d5a7cb41a430b5c
Venislav Kirilov <60657151+venipi4a@users.noreply.github.com>
2021-04-01 13:16:52 +0300

    Add files via upload

---
+++ Coordinates.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Coordinates {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("xa = ");
+        int xa = scan.nextInt();
+        System.out.println("ya = ");
+        int ya = scan.nextInt();
+        System.out.println("xb = ");
+        int xb = scan.nextInt();
+        System.out.println("yb = ");
+        int yb = scan.nextInt();
+
+        int A = xa + ya;
+        int B = xb + yb;
+
+        int length = Math.abs(A - B);
+        System.out.println(length);
+    }
+}
