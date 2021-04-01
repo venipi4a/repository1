814669b4be02d282f328abe1af2a9307f6971215
Venislav Kirilov <60657151+venipi4a@users.noreply.github.com>
2021-04-01 14:11:16 +0300

    Update Coordinates.java

--- Coordinates.java
+++ Coordinates.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class Coordinates {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
         System.out.println("xa = ");
         int xa = scan.nextInt();
+        
         System.out.println("ya = ");
         int ya = scan.nextInt();
+        
         System.out.println("xb = ");
         int xb = scan.nextInt();
+        
         System.out.println("yb = ");
         int yb = scan.nextInt();
 
