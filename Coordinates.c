de09d7b69363930924988dc0fab46d1ee23df619
Venislav Kirilov <60657151+venipi4a@users.noreply.github.com>
2021-04-01 13:20:54 +0300

    Add files via upload

---
+++ Coordinates.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int xa, ya, xb, yb ,x ,y;
+    printf("xa = ");
+    scanf("%d", &xa);
+    printf("ya ");
+    scanf("%d", &ya);
+    printf("xb = ");
+    scanf("%d", &xb);
+    printf("yb = ");
+    scanf("%d", &yb);
+    x = xb + xy;
+    y = ya + yb
+    printf("%d + %d = %d\n", a, b, c);
+    if(x <= y)
+    {
+        printf(y - x)
+    }
+    if(y <= x)
+    {
+        printf(x - y)
+    }
+}
